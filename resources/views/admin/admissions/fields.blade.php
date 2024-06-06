4c149c409d0ee2c4f9500c27d9ce206e78747bd3
Clifford537 <cliffordmukosh@gmail.com>
2024-06-06 19:08:14 +0300

    update on validation

--- resources/views/admin/admissions/fields.blade.php
+++ resources/views/admin/admissions/fields.blade.php
@@ -3,61 +3,52 @@
     {!! Form::label('patient_id', 'Patient:') !!}
     {!! Form::select('patient_id', $patients->mapWithKeys(function ($patient) {
         return [$patient->id => $patient->first_name . ' ' . $patient->surname . ' ' . $patient->other_names];
-    })->prepend('Select Patient', ''), null, ['class' => 'form-control', 'required']) !!}
+    })->prepend('Select Patient', ''), null, ['class' => 'form-control', 'required', 'placeholder' => 'Select Patient']) !!}
 </div>
-
 
 <!-- Admission Date Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('admission_date', 'Admission Date:') !!}
-    {!! Form::text('admission_date', null, ['class' => 'form-control','id'=>'admission_date']) !!}
+    {!! Form::text('admission_date', null, ['class' => 'form-control', 'id' => 'admission_date', 'placeholder' => 'Select Admission Date']) !!}
 </div>
+
 @push('page_scripts')
-    <!-- Include jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- Include Bootstrap Datepicker JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <!-- Include flatpickr CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+    <!-- Include flatpickr JS -->
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
     <script>
-        $(document).ready(function(){
-            $('#admission_date').datepicker({
-                format: 'mm/dd/yyyy',
-                autoclose: true
-            });
+        flatpickr('#admission_date', {
+            dateFormat: "Y-m-d",
+            minDate: "today"
         });
-    </script>
-@endpush
-
-
-@push('page_scripts')
-    <script type="text/javascript">
-        $('#admission_date').datepicker()
     </script>
 @endpush
 
 <!-- Doctor Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('doctor_id', 'Doctor:') !!}
-    {!! Form::Select('doctor_id', $doctors->mapWithKeys(function($doctor){
-        return[$doctor->id => $doctor->doctor_first_name .' '. $doctor->surname .' ' . $doctor->other_names];
-     }) ->prepend('Select Doctor',''), null, ['class' => 'form-control']) !!}
+    {!! Form::select('doctor_id', $doctors->mapWithKeys(function($doctor){
+        return [$doctor->id => $doctor->doctor_first_name .' '. $doctor->surname .' ' . $doctor->other_names];
+     })->prepend('Select Doctor',''), null, ['class' => 'form-control', 'placeholder' => 'Select Doctor']) !!}
 </div>
-
 
 <!-- Reason For Admission Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('reason_for_admission', 'Reason For Admission:') !!}
-    {!! Form::text('reason_for_admission', null, ['class' => 'form-control', 'maxlength' => 100, 'maxlength' => 100]) !!}
+    {!! Form::text('reason_for_admission', null, ['class' => 'form-control', 'maxlength' => 100, 'placeholder' => 'Enter Reason For Admission']) !!}
 </div>
 
 <!-- Discharge Status Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('discharge_status', 'Discharge Status:') !!}
-    {!! Form::text('discharge_status', null, ['class' => 'form-control', 'maxlength' => 100, 'maxlength' => 100]) !!}
+    {!! Form::text('discharge_status', null, ['class' => 'form-control', 'maxlength' => 100, 'placeholder' => 'Enter Discharge Status']) !!}
 </div>
 
 <!-- Admission Types Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('admission_types_id', 'Admission Types:') !!}
-    {!! Form::select('admission_types_id', $admission_types->pluck('type', 'id')->prepend('Select Admission Type', ''), null, ['class' => 'form-control', 'required']) !!}
-
+    {!! Form::select('admission_types_id', $admission_types->pluck('type', 'id')->prepend('Select Admission Type', ''), null, ['class' => 'form-control', 'required', 'placeholder' => 'Select Admission Type']) !!}
 </div>
